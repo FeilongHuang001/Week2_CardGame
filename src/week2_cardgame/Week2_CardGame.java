4304b4fa284630ccbdee0c74491aba9cd63094c2
Feilong Huang <felixh6888@gmail.com>
2019-05-13 15:22:54 -0400

    First practice

---
+++ src/week2_cardgame/Week2_CardGame.java
@@ -0,0 +1,15 @@
+package week2_cardgame;
+
+/**
+ * @author Feilong Huang  <felixh6888 at gmail.com>
+ */
+public class Week2_CardGame
+{
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      System.out.println("This is a great card game!");
+   }
+}
